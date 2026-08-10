ee0d80c7ab58710f3dca8bbfe5f86be0878423aa
s1nju <78903844+amine502@users.noreply.github.com>
2026-08-10 22:23:08 +0100

    fix:edit mute to onot delete all user roles

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -77,12 +77,15 @@ class Moderation(commands.Cog):
                                 
                         elif cmd_prefix == aliases.get('chat_mute', 'اسكت'):
                             mute_role = discord.utils.get(message.guild.roles, name="Muted")
+                            rmd_role = discord.utils.get(message.guild.roles, name="RMD")
                             if not mute_role:
                                 mute_role = await message.guild.create_role(name="Muted")
                                 for ch in message.guild.text_channels:
                                     await ch.set_permissions(mute_role, send_messages=False)
+                            if not rmd_role:
+                                rmd_role = await message.guild.create_role(name="RMD")
             
-                            await target_member.edit(roles= {mute_role}, reason=f"Chat Mute by {message.author}")
+                            await target_member.edit(roles= {mute_role, rmd_role}, reason=f"Chat Mute by {message.author}")
                             await message.channel.send(f"تم اسكات {target_member.mention}")
                                 
                         elif cmd_prefix == aliases.get('chat_unmute', 'تكلم'):
