bcf22b6155a350d270b72342e45b0e1c33da24cb
s1nju <78903844+amine502@users.noreply.github.com>
2026-08-21 17:31:02 +0100

    setting up a leveling message channel

--- cogs/leveling.py
+++ cogs/leveling.py
@@ -72,13 +72,13 @@ class Leveling(commands.Cog):
 
             # Send level up message
             embed = EmbedFactory.level_up(message.author, new_level, new_xp)
-            level_up_channel = self.module_config.get('level-systemᴿᴹᴰ')
-            if level_up_channel:
-                channel = message.guild.get_channel(level_up_channel)
-                if channel:
-                    await channel.send(embed=embed)
-            else:
-                await message.channel.send(embed=embed)
+            # Default to the specific channel ID requested if not in config
+            level_up_channel_id = self.module_config.get('level_up_channel', 1537828641071693956)
+            channel = message.guild.get_channel(level_up_channel_id)
+            
+            if channel:
+                await channel.send(message.author.mention, embed=embed)
+            
             logger.info(f"{message.author} leveled up to {new_level} in {message.guild}")
         else:
             await self.db.update_user(message.author.id, message.guild.id, {'xp': new_xp})
